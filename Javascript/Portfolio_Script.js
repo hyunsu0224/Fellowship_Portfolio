a61b3112073e8acf588dabcd1fde92a70b883076
hyunsu0224 <zsdry19203@gmail.com>
2020-12-04 17:50:33 +0900

    12/04 commit

--- Javascript/Portfolio_Script.js
+++ Javascript/Portfolio_Script.js
@@ -1,29 +1,12 @@
-$(window).scroll(function() {
-
-    // selectors
-    var $window = $(window),
-        $body = $('body'),
-        $panel = $('.panel');
-    
-    // Change 33% earlier than scroll position so colour is there when you arrive.
-    var scroll = $window.scrollTop() + ($window.height() / 3);
-
-    $panel.each(function () {
-        var $this = $(this);
-
-      // if position is within range of this panel.
-      // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-      // Remember we set the scroll to 33% earlier in scroll var.
-    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-            
-        // Remove all classes on body with color-
-        $body.removeClass(function (index, css) {
-        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-        });
-    
-        // Add class of currently active div
-        $body.addClass('color-' + $(this).data('color'));
-    
-    });    
-    
-}).scroll();
+$(document).ready(function(){
+$('.sonbox').each(function(index){ 
+	$(this).attr('sonbox-index', index);
+}).click(function(){
+    /*클릭된 <div>의 menu-index 값을 index 변수에 할당한다.*/
+    var index = $(this).attr('son-box-index');
+    /*클릭한 <div>에  clicked_menu 클래스 추가*/
+	$('.sonbox[sonbox-index=' + index + ']').addClass('clicked_sonbox'); 
+    /*그 외 <div>는  clicked_menu 클래스 삭제*/
+	$('.sonbox[sonbox-index!=' + index + ']').removeClass('clicked_sonbox');
+});
+});
