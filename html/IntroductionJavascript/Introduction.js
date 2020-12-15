0d23e80df76f8d6935154b21ac18b8f51a7cd453
hyunsu0224 <zsdry19203@gmail.com>
2020-12-15 09:18:12 +0900

    12/15 first

--- html/IntroductionJavascript/Introduction.js
+++ html/IntroductionJavascript/Introduction.js
@@ -1,2 +1,29 @@
 window.history.back()
 window.history.forward()
+/*
+var imgArray = new Array();
+imgArray[0] = "../IntroductionImage/Atom.PNG";
+imgArray[1] = "../IntroductionImage/bluebird.PNG"; 
+imgArray[2] = "../IntroductionImage/instagram.PNG"; 
+imgArray[3] = "../IntroductionImage/beautifulstore.PNG"; 
+
+function showImage(){ 
+    var imgNum = Math.round(Math.random()*3);
+    var objImg = document.getElementById("introimg");
+    objImg.src = imgArray[imgNum];
+    setTimeout(showImage,5000); 
+}
+
+
+$(document).ready(function () {
+	// 이미지 width 250 변경. attr()
+	$('img').attr('width', '250');
+	//$('img').css('width', '250px');
+	setInterval(function () {
+		$('img').first().appendTo('div');   // 제일 첫번째 이미지를 div 태그 제일 뒤에 붙힌다.
+	}, 2000);
+	setTimeout(function () {
+		alert('aaa')
+	}, 5000);
+});
+*/
