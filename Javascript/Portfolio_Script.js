e2a4eb0f6d8a82bbfaf2d9933d5f529ed788f7c1
hyunsu0224 <zsdry19203@gmail.com>
2020-12-23 18:09:16 +0900

    12/23 Wed

--- Javascript/Portfolio_Script.js
+++ Javascript/Portfolio_Script.js
@@ -1,15 +1,2 @@
-$(document).ready(function(){
-$('.sonbox').each(function(index){ 
-	$(this).attr('sonbox-index', index);
-}).click(function(){
-    /*클릭된 <div>의 menu-index 값을 index 변수에 할당한다.*/
-    var index = $(this).attr('son-box-index');
-    /*클릭한 <div>에  clicked_menu 클래스 추가*/
-	$('.sonbox[sonbox-index=' + index + ']').addClass('clicked_sonbox'); 
-    /*그 외 <div>는  clicked_menu 클래스 삭제*/
-	$('.sonbox[sonbox-index!=' + index + ']').removeClass('clicked_sonbox');
-});
-});
-
 window.history.back()
 window.history.forward()
